60382e36c43fd42ed18835f31fbd10bc7685dde4
Richard do Nascimento Fagundes <richardnfag@outlook.com>
2023-04-23 10:44:31 -0300

    Add Test Coverage

--- test.py
+++ test.py
@@ -2,13 +2,14 @@
 from app import app
 import unittest
 
+
 class Test(unittest.TestCase):
     def setUp(self):
         # cria uma instância do unittest, precisa do nome "setUp"
         self.app = app.test_client()
 
         # envia uma requisicao GET para a URL
-        self.result = self.app.get('/')
+        self.result = self.app.get("/")
 
     def test_requisicao(self):
         # compara o status da requisicao (precisa ser igual a 200)
@@ -16,4 +17,6 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Laboratório Pipeline DevOps")
+        self.assertEqual(
+            self.result.data.decode("utf-8"), "Laboratório Pipeline DevOps"
+        )
