39ea0347a8a81b295d346e57d41b5d9eb204e198
Richard do Nascimento Fagundes <richardnfag@outlook.com>
2023-04-23 14:44:13 -0300

    Add Docker Containerization

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
+from os import getenv
 
 app = Flask(__name__)
 
 csrf = CSRFProtect(app)
+
 
 @app.route("/")
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(getenv("PORT", "80"))
+    app.run("0.0.0.0", port=port)
